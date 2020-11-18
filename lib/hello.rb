e94aa0cd0f9968d1126084a4a5188f23a6abbf58
Hannah Malm <hannahemalm616@gmail.com>
2020-11-18 01:42:04 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -12,12 +12,12 @@ def hello_t(array)
   end
 end
 
-# call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+# # call your method here!
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
 
 
 #why do we use the while loop?
